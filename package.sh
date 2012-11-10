50a0ef890f2ab5b5829bbafd4a738286ba6e5d0b
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-11 00:35:44 +0100

    Add zip

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir build -p
 tar czvf build/doctrine2-workshop.tar.gz . --exclude build --exclude .git --exclude cache --exclude logs --exclude *.swp --exclude workshop.db
+tar czvf build/doctrine2-workshop.zip . --exclude build --exclude .git --exclude cache --exclude logs --exclude *.swp --exclude workshop.db
