6293e66fc4d851350282e97aa3c2855bd0c8e4c3
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-10 23:38:32 +0100

    Move route help action (homepage) to other controller to not auto routed.

---
+++ src/Doctrine/WorkshopBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Doctrine\WorkshopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $reflection = new \ReflectionObject($this);
+        $routeNames = array();
+
+        foreach ($reflection->getMethods() as $method) {
+            if (substr($method->getName(), -6) == "Action") {
+                $routeNames[] = strtolower(str_replace("Action", "", $method->getName()));
+            }
+        }
+
+        return $this->render('DoctrineWorkshopBundle:Default:index.html.twig', array(
+            'routeNames' => $routeNames,
+        ));
+    }
+}
+
+
--- src/Doctrine/WorkshopBundle/Controller/WorkshopController.php
+++ src/Doctrine/WorkshopBundle/Controller/WorkshopController.php
@@ -10,18 +10,7 @@ class WorkshopController extends Controller
 {
     public function indexAction()
     {
-        $reflection = new \ReflectionObject($this);
-        $routeNames = array();
-
-        foreach ($reflection->getMethods() as $method) {
-            if (substr($method->getName(), -6) == "Action") {
-                $routeNames[] = strtolower(str_replace("Action", "", $method->getName()));
-            }
-        }
-
-        return $this->render('DoctrineWorkshopBundle:Workshop:index.html.twig', array(
-            'routeNames' => $routeNames,
-        ));
+        return $this->render('DoctrineWorkshopBundle:Workshop:index.html.twig', array());
     }
 }
 
