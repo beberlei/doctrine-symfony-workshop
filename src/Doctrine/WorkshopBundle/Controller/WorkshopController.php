bbf0376652b3608890c3b37c2cc9806155917332
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-10 23:32:43 +0100

    Implement AutoRouteLoader and commit Vehicle Entity

--- src/Doctrine/WorkshopBundle/Controller/WorkshopController.php
+++ src/Doctrine/WorkshopBundle/Controller/WorkshopController.php
@@ -10,8 +10,17 @@ class WorkshopController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DoctrineWorkshopBundle:Workshop:index.html.twig', array(
+        $reflection = new \ReflectionObject($this);
+        $routeNames = array();
 
+        foreach ($reflection->getMethods() as $method) {
+            if (substr($method->getName(), -6) == "Action") {
+                $routeNames[] = strtolower(str_replace("Action", "", $method->getName()));
+            }
+        }
+
+        return $this->render('DoctrineWorkshopBundle:Workshop:index.html.twig', array(
+            'routeNames' => $routeNames,
         ));
     }
 }
