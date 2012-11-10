dc8dc3df1b7b3388b7118eba21a38b9a964b446f
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-11 00:03:22 +0100

    exclude db as well

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir build -p
-tar czvf build/doctrine2-workshop.tar.gz . --exclude build --exclude .git --exclude cache --exclude logs --exclude *.swp
+tar czvf build/doctrine2-workshop.tar.gz . --exclude build --exclude .git --exclude cache --exclude logs --exclude *.swp --exclude workshop.db
